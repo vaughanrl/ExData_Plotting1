9949ed811dbde83d89ec47b05b199a94008fe230
Ron Vaughan <ronaldlvaughan@gmail.com>
2017-10-16 01:08:28 -0400

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -33,7 +33,7 @@ with(powerData,
             xlab="")       
        plot(Sub_metering_1~Datetime,
             type="l",
-            ylab="Global Active Power (kilowatts)",
+            ylab="Energy sub metering",
             xlab="")       
        lines(Sub_metering_2~Datetime, col='Red')
        lines(Sub_metering_3~Datetime, col='Blue')
@@ -42,7 +42,7 @@ with(powerData,
               lty=1,
               lwd=2,
               bty="n",
-              legend=c("Sub_metering_1", "Sub_meering_2", "Sub_metering_3"))
+              legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
        plot(Global_reactive_power~Datetime,
             type="l",
             ylab="Global Reactive Power (kilowatts)",
